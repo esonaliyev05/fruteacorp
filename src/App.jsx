e03934240d1793b49219d8a208d3087db95d1ccf
Alyorbekjonxon <alyorbek391@gmail.com>
2025-03-07 00:01:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import HomePage from './Pages/home/HomePage'
+import Like from './Pages/Like/Like'
+
+const App = () => {
+  return (
+    <>
+         <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+
+          <Route index element={<HomePage/>} />
+          
+          <Route path="like" element={<Like />} />
+          {/* <Route path="brands" element={<BrandsPage />} /> */}
+          {/* <Route path="brands/:id" element={<SingleBrandPage />} />
+          <Route path="services/uslugbuggies" element={<UslugBuggiesPage />} />
+          <Route path="services/uslugcar" element={<UslugCarPage />} />
+          <Route path="services" element={<ServicePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="blog/singleblog" element={<SingleBlogPage />} />
+          <Route path="blog/singleblog2" element={<SingleBlogPage2 />} />
+          <Route path="blog/singleblog3" element={<SingleBlogPage3 />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Savol-javob/Faq.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from '../../components/footer/Footer'
+
+const Faq = () => {
+  return (
+   <div>
+    
+   </div>
+  )
+}
+
+export default Faq
