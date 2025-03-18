de0aec9657f817d2a4c3b8a56747e50857114247
esonaliyev05 <esonaliyevalyorbek911@gmail.com>
2025-03-18 07:38:23 +0500

    time notfount

--- src/Pages/NotFount/NotFount.jsx
+++ src/Pages/NotFount/NotFount.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NotFount = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval); // Komponent unmount bo'lganda intervalni tozalash
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
     <h1 className="text-8xl font-bold text-red-600">404</h1>
     <p className="text-2xl text-gray-600 mt-4">Sahifa topilmadi!</p>
     <p className="text-lg text-gray-600 mt-2">Siz qidirayotgan sahifa mavjud emas yoki olib tashlangan.</p>
     <a href="/" className="mt-6 text-lg p-[8px] bg-green-600 text-white rounded-[5px]">Bosh sahifaga qaytish</a>
+    <div style={{ fontSize: "24px", fontWeight: "bold", textAlign: "center", marginTop: "20px" }}>
+      {time.toLocaleTimeString()}
+    </div>
   </div>
   )
-}
+  }
+
 
 export default NotFount
+
+// import React, { useState, useEffect } from "react";
+
+
+
+//   return (
+
+//   );
+// };
+
+// export default RealTimeClock;
